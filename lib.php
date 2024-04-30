bebfe6dce476f7e8fb1701abddde57e473359aa2
sreenivasmalae <sreenivas@human-logic.com>
2024-04-30 17:16:47 +0400

    Server URL can be updated by client

--- lib.php
+++ lib.php
@@ -69,7 +69,6 @@ function local_levitate_curlcall($fnname = '', $jsondata='') {
     global $CFG;
     $tokensettings = get_config('local_levitate');
     $tokenid = $tokensettings->secret;
-    
     $serverurl = $tokensettings->server_url.'/webservice/rest/server.php?wstoken=';
     $url = $serverurl.$tokenid.'&wsfunction='.$fnname.'&moodlewsrestformat=json';
     $curl = new curl();
@@ -77,10 +76,10 @@ function local_levitate_curlcall($fnname = '', $jsondata='') {
     if (isset(json_decode($response)->errorcode)) {
         redirect(new moodle_url('/admin/settings.php?section=locallevitategettoken'), get_string('invalidtoken', 'local_levitate'));
     }
-    if(json_decode($response)==null){
-       redirect(new moodle_url('/admin/settings.php?section=locallevitategettoken'), get_string('no_response_found', 'local_levitate', new moodle_url('/admin/settings.php?section=locallevitategettoken')));
+    if(json_decode($response) == null){
+        redirect(new moodle_url('/admin/settings.php?section=locallevitategettoken'),
+            get_string('no_response_found', 'local_levitate', new moodle_url('/admin/settings.php?section=locallevitategettoken')));
     }
-    
     return $response;
 }
 /**
